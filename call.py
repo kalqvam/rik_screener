44b0b2ab0736c5043e4cd150fad69c7e90dba4d9
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-26 15:06:46 +0300

    Update call.py

--- call.py
+++ call.py
@@ -1,4 +1,10 @@
+import sys
 import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
+
 import glob
 import pandas as pd
 import numpy as np
