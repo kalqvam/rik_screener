057249addf6e7e7b3ae4fb2e062034b134d70250
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-26 13:46:22 +0300

    Update call.py

--- call.py
+++ call.py
@@ -9,6 +9,13 @@ from datetime import datetime
 BASE_PATH = "/content/drive/MyDrive/Python/rik_screener"
 TIMESTAMP = datetime.now().strftime("%Y%m%d_%H%M%S")
 
+from data_preparation import filter_companies
+from multi_year_merger import merge_multiple_years
+from calculations import calculate_ratios
+from industry_codes import add_industry_classifications
+from shareholder_data import add_ownership_data
+from filtering import filter_and_rank
+
 try:
     from google.colab import drive
     drive.mount('/content/drive')
