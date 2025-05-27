6d302cf5f2079041ee892f7e67034f6b0c54f0ad
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-27 15:03:48 +0300

    Update __init__.py

--- rik_screener/post_processing/__init__.py
+++ rik_screener/post_processing/__init__.py
@@ -1,7 +1,7 @@
-from .emtak_assignment import assign_emtak_codes  # Assuming this exists
+#from .emtak_assignment import assign_emtak_codes  # Assuming this exists
 from .filtering import filter_and_rank
 
 __all__ = [
-    'assign_emtak_codes',
+    #'assign_emtak_codes',
     'filter_and_rank'
 ]
