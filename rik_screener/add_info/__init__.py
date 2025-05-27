d2e714b0689455d588212325ae289cd70feb9ce4
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-27 14:48:32 +0300

    Update __init__.py

--- rik_screener/add_info/__init__.py
+++ rik_screener/add_info/__init__.py
@@ -1,1 +1,7 @@
+from .industry_codes import add_industry_classifications
+from .shareholder_data import add_ownership_data
 
+__all__ = [
+    'add_industry_classifications',
+    'add_ownership_data'
+]
