96564c5ebe870d2fa271848ec188572ae10c5d9f
kalqvam <73297077+kalqvam@users.noreply.github.com>
2025-05-28 23:38:27 +0300

    Update __init__.py

--- rik_screener/add_info/__init__.py
+++ rik_screener/add_info/__init__.py
@@ -1,10 +1,12 @@
 from .industry_codes import add_industry_classifications
 from .shareholder_data import add_ownership_data
 from .emtak_descriptions import add_emtak_descriptions, get_industry_summary
+from .company_age import add_company_age
 
 __all__ = [
     'add_industry_classifications',
     'add_ownership_data',
     'add_emtak_descriptions',
-    'get_industry_summary'
+    'get_industry_summary',
+    'add_company_age'
 ]
